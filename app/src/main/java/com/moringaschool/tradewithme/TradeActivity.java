afee3c3195c6ccaa2cb51022e3968935b61c9dd4
BillyAyiera <ayierabilly1@gmail.com>
2020-05-25 09:59:26 +0300

    added an Array

--- app/src/main/java/com/moringaschool/tradewithme/TradeActivity.java
+++ app/src/main/java/com/moringaschool/tradewithme/TradeActivity.java
@@ -4,16 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TradeActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] trade = new String[] {"Electronic Shop", "Jumia",
+            "Kilimall", "Phone Shop", "Luca", "Basil",
+            "Billy", "Cake Shop", "Naivas", "Phone Repair",
+            "Kevin"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, trade);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(TradeActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("People Trading In: " + location);
